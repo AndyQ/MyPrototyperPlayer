49dcb6d41f9839fb86cb10aa075ef3bb07a9804b
AndyQ <andy.qua@gmail.com>
2014-02-03 14:43:07 +0000

    Initial Commit

---
+++ PrototyperPlayback/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PrototyperPlayback
+//
+//  Created by Andy Qua on 03/02/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PrototyperPlayback/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PrototyperPlayback
+//
+//  Created by Andy Qua on 03/02/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
